f0b2f60a486f754bb97ee10c9e344c8ed73ba5f1
Mohamed Ali Khaskia <mohamedalik88@gmail.com>
2017-05-27 04:03:43 +0200

    firstCommit

---
+++ Main.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace AttributeGrammer {
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtGrammarRules = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtProductionRules = new System.Windows.Forms.TextBox();
+            this.listAtoms = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.button1.FlatAppearance.BorderSize = 8;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Silver;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button1.Location = new System.Drawing.Point(68, 75);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(502, 72);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Calculate Attribute Grammar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtGrammarRules
+            // 
+            this.txtGrammarRules.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGrammarRules.Location = new System.Drawing.Point(68, 35);
+            this.txtGrammarRules.Name = "txtGrammarRules";
+            this.txtGrammarRules.Size = new System.Drawing.Size(502, 34);
+            this.txtGrammarRules.TabIndex = 1;
+            this.txtGrammarRules.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label1.Location = new System.Drawing.Point(65, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Grammar Rules :";
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.button2.FlatAppearance.BorderSize = 8;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Silver;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button2.Location = new System.Drawing.Point(68, 227);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(502, 76);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Calculate the Atoms";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label2.Location = new System.Drawing.Point(65, 167);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "The Production :";
+            // 
+            // txtProductionRules
+            // 
+            this.txtProductionRules.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtProductionRules.Location = new System.Drawing.Point(68, 187);
+            this.txtProductionRules.Name = "txtProductionRules";
+            this.txtProductionRules.Size = new System.Drawing.Size(502, 34);
+            this.txtProductionRules.TabIndex = 6;
+            this.txtProductionRules.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // listAtoms
+            // 
+            this.listAtoms.Font = new System.Drawing.Font("Segoe Marker", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listAtoms.ForeColor = System.Drawing.Color.DimGray;
+            this.listAtoms.FormattingEnabled = true;
+            this.listAtoms.ItemHeight = 24;
+            this.listAtoms.Location = new System.Drawing.Point(68, 338);
+            this.listAtoms.Name = "listAtoms";
+            this.listAtoms.Size = new System.Drawing.Size(502, 124);
+            this.listAtoms.TabIndex = 7;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(645, 479);
+            this.Controls.Add(this.listAtoms);
+            this.Controls.Add(this.txtProductionRules);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtGrammarRules);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "MainForm";
+            this.Text = "Attribute Grammer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtGrammarRules;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtProductionRules;
+        private  System.Windows.Forms.ListBox listAtoms;
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AttributeGrammer {
+    public partial class MainForm : Form {
+
+
+
+        public MainForm()
+        {
+            InitializeComponent();
+            txtGrammarRules.Text = "*-/*+abcdef";
+            this.Height = 200;
+        }
+
+
+        public static bool Isoperator(String s) {
+            if (s[0] == '*' || s[0] == '/' || s[0] == '$'
+                || s[0] == '%' || s[0] == '+' || s[0] == '-') {
+            return true;
+        }
+        return false;
+        }
+
+        
+
+        // Prefix To Infix 
+        public static String preToIn(String s) {
+        ArrayStack stack = new ArrayStack(20);
+        String a, b, test;
+        for (int i = 0; i != s.Length; i++) {
+            test = "" + s[i];
+            if (Isoperator(test)) {
+                stack.push(test);
+            } else {
+                if (!Isoperator(stack.gettop())) {
+                    a = stack.pop();
+                    b = stack.pop();
+                    test = "" + a + b + test + "";
+                }
+                stack.push(test);
+            }
+        }
+        return stack.pop();
+    }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtProductionRules.Text = preToIn(txtGrammarRules.Text);
+            this.Height = 380;
+            
+        }
+        static String s;
+        static int temp = 0;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listAtoms.Items.Clear();
+            temp = 0;
+            s =txtProductionRules.Text;
+
+          //  List<String> pluses = new List<String>();
+            List<String> Result = new List<String>();
+
+             getResult(Result);
+           
+            //catch
+            //{
+            //    MessageBox.Show("Incorrect ");
+            //}
+            foreach (var item in Result)
+            {
+                listAtoms.Items.Add(item);
+            }
+            this.Height = 600;
+
+
+
+
+        }
+         void getResult(List<String> Result)
+        {
+
+            // Perform Multiplication and Divition
+            for (int i = 1; i < s.Length; i++)
+            {
+                // perform multiplication First : 
+
+                if (s[i] == '*')
+                {
+                    if (Isoperator(  s[i-1].ToString()) || Isoperator(s[i+1].ToString())){
+                        MessageBox.Show(" Incorrect input"); listAtoms.Items.Clear();
+
+                        return;
+                    }
+                    
+                    Result.Add("MUL  " + s[i - 1] + " " + s[i + 1] + "  ==> " + temp);
+                    s = s.Replace(s[i - 1] + "*" + s[i + 1], Convert.ToString(temp++));
+                    i = 0;
+                    
+                }
+                else if (s[i] == '/')
+                {
+                    if (Isoperator(  s[i-1].ToString()) || Isoperator(s[i+1].ToString())){
+                        MessageBox.Show(" Incorrect input");
+                        return;
+                    }
+                    Result.Add("Div  " + s[i - 1] + " " + s[i + 1] + "  ==> " + temp);
+                    s = s.Replace(s[i - 1] + "/" + s[i + 1],  Convert.ToString(temp));
+                    i = 0;
+                    temp++;
+                }
+
+            }
+            // PrintRes();
+            // Perform Addition
+            for (int i = 1; i < s.Length; i++)
+            {
+
+                // perform multiplication First : 
+                if (s[i] == '+')
+                {
+                    if (Isoperator(  s[i-1].ToString()) || Isoperator(s[i+1].ToString())){
+                        MessageBox.Show(" Incorrect input"); listAtoms.Items.Clear();
+
+                        
+                        return;
+                    }
+
+                    Result.Add("ADD  " + s[i - 1] + " " + s[i + 1] + "  ==> " + temp);
+                    s = s.Replace(s[i - 1] + "+" + s[i + 1],  Convert.ToString(temp));
+                    i = 0;
+                    temp++;
+                }
+                else if (s[i] == '-')
+                {
+                    if (Isoperator(  s[i-1].ToString()) || Isoperator(s[i+1].ToString())){
+                        MessageBox.Show(" Incorrect input"); listAtoms.Items.Clear();
+
+                        return;
+                    }
+
+                    Result.Add("Sub  " + s[i - 1] + " " + s[i + 1] + "  ==> " + temp);
+                    s = s.Replace(s[i - 1] + "-" + s[i + 1],  Convert.ToString(temp));
+                    i = 0;
+                    temp++;
+                }
+            }
+            
+        }
+       
+
+
+
+
+
+    }
+}
